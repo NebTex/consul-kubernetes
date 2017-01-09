d7cb35e1ace09e0f874058267b1161edf4555293
Cristian Lozano <criscalovis@gmail.com>
2017-01-09 00:23:39 -0500

    generate acl tokens randomly

--- launch.sh
+++ launch.sh
@@ -6,10 +6,10 @@ curl -fsSL https://github.com/gliderlabs/sigil/releases/download/v0.4.0/sigil_0.
 if kubectl get secrets --namespace kv | grep -q "consul-secrets"; then
     echo "Secrets already exists"
 else
-    /tmp/sigil -p -f consul-secrets.yml  acl_master_token=`uuidgen` \
+    sigil -p -f consul-secrets.yml secret="$(/tmp/sigil -p -f consul.json  acl_master_token=`uuidgen` \
 acl_replication_token=`uuidgen` acl_agent_master_token=`uuidgen` \
 acl_agent_token=`uuidgen` acl_token=`uuidgen` \
-| kubectl  apply --validate --overwrite -f -I
+| base64 -w 0)" | kubectl  apply --validate --overwrite -f -
 fi
 
 # ====================
