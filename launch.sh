040ede5227afbab644d3c74b52442cececd9134f
Cristian Lozano <criscalovis@gmail.com>
2017-01-30 07:15:02 -0500

    add backup to leader

--- launch.sh
+++ launch.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+AWS_BUCKET=`cat ${VAULT_BACKUP} | jq .AWS_BUCKET -r | base64 -w 0`
+AWS_REGION=`cat ${VAULT_BACKUP} | jq .AWS_REGION -r | base64 -w 0`
+AWS_ACCESS_KEY_ID=`cat ${VAULT_BACKUP} | jq .AWS_ACCESS_KEY_ID -r | base64 -w 0`
+AWS_SECRET_ACCESS_KEY=`cat ${VAULT_BACKUP} | jq .AWS_SECRET_ACCESS_KEY -r | base64 -w 0`
+BORG_PASSPHRASE=`cat ${VAULT_BACKUP}| jq .BORG_PASSPHRASE -r | base64 -w 0`
 
 if kubectl get secrets --namespace kv | grep -q "consul-secrets"; then
     echo "Secrets already exists"
@@ -9,6 +16,11 @@ acl_agent_token=`uuidgen` acl_token=`uuidgen` \
 | base64 -w 0)" | kubectl  apply --validate --overwrite -f -
 fi
 
+
+sigil -p -f consul-backup-credentials.yml AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION \
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    BORG_PASSPHRASE=$BORG_PASSPHRASE  | kubectl  apply --validate --overwrite -f -
+ 
 # ====================
 #       Leader
 # ====================
@@ -20,5 +32,5 @@ kubectl apply -f consul-leader-service.yml --validate --overwrite
 #       Replicas
 # ====================
 
-kubectl apply -f consul-statefulsets.yml --validate --overwrite
-kubectl apply -f consul-service.yml --validate --overwrite
+#kubectl apply -f consul-statefulsets.yml --validate --overwrite
+#kubectl apply -f consul-service.yml --validate --overwrite
